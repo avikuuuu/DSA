bf05bf08412dc1314fafa03aad2bb103a02fa50b
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-01-31 21:52:24 +0530

    day 30 jan done

--- 02-bitwise operator/leetcode-1009.cpp
+++ 02-bitwise operator/leetcode-1009.cpp
@@ -2,36 +2,34 @@
 #include <cmath> // Include the cmath header for pow function
 
 int main() {
-    int ans = 0, i = 0,j=0, show = 0;
+    int ans = 0,rev=0, i = 0,j=0, show = 0;
     int n = 5;
 
     // Convert binary to decimal
     while (n) {
         int digit = n & 1;
         // int rev = ~digit;
-        ans = (digit * pow(10, i)) + ans;
+        ans += (digit * pow(10, i));
         n = n >> 1;
         i++;
     }
     std::cout << ans << " before decimal" << std::endl;
 
-    int rev=0;
-
-    while(ans){
-        int mod=ans%10;
-        if(mod==1){
-            rev=(rev*10)+0;
-        } else if(mod==0){
-            rev=(rev*10)+1;
+    while (ans) {
+        int mod = ans % 10;
+        if (mod == 1) {
+            rev = rev * 10 + 0;
+        } else if (mod == 0) {
+            rev = rev * 10 + 1;
         }
-        ans/=10;
+        ans /= 10;
     }
     std::cout << rev << " before decimal" << std::endl;
     // Convert decimal back to binary
     while (rev) {
         int mod = rev % 10;
         if (mod == 1) {
-            show = rev+ pow(2, j);
+            show = show + pow(2, j);
         }
         rev = rev/10;
         j++;
--- try.cpp
+++ try.cpp
@@ -1,113 +1,44 @@
-#include<iostream>
-#include<conio.h>
-#include<time.h>
-#include<dos.h>
-#include<stdlib.h>
+#include <iostream>
+#include <cmath> // Include the cmath header for pow function
 
-int a[1000],p,q,r,n,ch=1,a1;
+int main() {
+    int ans = 0, i = 0, j = 0, show = 0;
+    int n = 10;
 
-void quickSort(int[],int,int);
-void allocate(int,int);
-int partition(int[],int,int);
-
-void main()
-{
-  clrscr();
-  randomize();
-  do{
-    cout<<"\nEnter Size of array : ";
-    cin>>n;
-    cout<<"\n1. Average Case\n2. Worst Case\nEnter Your Choice-->";
-    cin>>a1;
-    switch(a1)
-    {
-      case 1: cout<<"\nAVERAGE CASE\n";
-      allocate(n,a1);
-      p=0;
-      r=n-1;
-      clock_t start,end;
-      start=clock();
-      quickSort(a,p,r);
-      end=clock();
-      cout<<"\nThe time is : "<<(end-start)/CLK_TCK;
-      cout<<"\nSorted Array is : \n";
-      for(int i=0;i<n;i++)
-      cout<<a[i]<<" ";
-      break;
-
-      case 2: cout<<"\nWORST CASE\n";
-      allocate(n,a1);
-      p=0;
-      r=n-1;
-      start=clock();
-      quickSort(a,p,r);
-      end=clock();
-      cout<<"\nThe time is : "<<(end-start)/CLK_TCK;
-      break;
-     }
-
-       cout << "AVDESH KUMAR \n";
-        cout << "IT - 3 \n";
-        cout << "00176807721 \n";
-      cout<<"Press '1' to calculate other running time --> ";
-      cin>>ch;
-    }while(ch==1);
-    getch();
-}
-
-void allocate(int n,int a1)
-{
-  if(a1==1)
-  {
-    for(int i=0;i<n;i++)
-    {
-      a[i]=random(n);
+    // Convert decimal to binary
+    while (n) {
+        int digit = n % 2;
+        ans = ans + digit * pow(10, i);
+        n = n / 2;
+        i++;
     }
-   }
-   if(a1==2)
-   {
-     for(int i=0;i<n;i++)
-     {
-       a[i]=i;
-     }
-   }
-}
+    std::cout << ans << " before reversing" << std::endl;
 
-void quickSort(int a[],int p,int r)
-{
-  if(p<r)
-  {
-    delay(1.2);
-    int q = partition(a,p,r);
-    quickSort(a,p,q);
-    quickSort(a,q+1,r);
-  }
-}
+    // Reverse the binary by swapping 0s and 1s
+    int rev = 0;
+    while (ans) {
+        int mod = ans % 10;
+        if (mod == 1) {
+            rev = rev * 10 + 0;
+        } else if (mod == 0) {
+            rev = rev * 10 + 1;
+        }
+        ans /= 10;
+    }
+    std::cout << rev << " after reversing" << std::endl;
 
-int partition(int a[],int p,int r)
-{
-   int x = a[p];
-   int i = p-1;
-   int j = r+1;
-   while(1)
-   {
-     do
-     {
-       j = j-1;
-     }while(a[j]>x);
-     do
-     {
-       i =i+1;
-     }while(a[i]<x);
-     if(i<j)
-     {
-       int temp=a[i];
-       a[i]=a[j];
-       a[j]=temp;
-     }
-     else
-     {
-       return j;
-     }
-   }
+    // Convert binary back to decimal
+    while (rev) {
+        int mod = rev % 10;
+        if (mod == 1) {
+            show = show + pow(2, j);
+        }
+        rev = rev / 10;
+        j++;
+    }
+
+    // Add a line to print the result
+    std::cout << show << " after converting back to decimal" << std::endl;
+
+    return 0;
 }
