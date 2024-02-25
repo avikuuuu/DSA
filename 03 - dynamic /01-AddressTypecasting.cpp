7706434f10176269b229fb39e4601b16e9c69e70
Avdesh kumar <70461207+avikuuuu@users.noreply.github.com>
2024-02-25 22:23:10 +0530

    chnge

--- 03 - dynamic /01-AddressTypecasting.cpp
+++ 03 - dynamic /01-AddressTypecasting.cpp
@@ -8,7 +8,7 @@ int main() {
     cout << c << endl; // Outputs 'A'
 
     // Part 2: Casting int pointer to char pointer
-    int *p = &i; //have address of i
+    int  *p = &i; //have address of i
     char *pc = (char*)p; //expecit type cast
 
     cout << p << endl;  // Outputs the address of i
@@ -17,9 +17,5 @@ int main() {
     // Part 3: Dereferencing int and char pointers
     cout << *p << endl;     // Outputs the value of i (65)
     cout << *pc << endl;    // Outputs 'A' (the character representation of the first byte of i)
-    cout << *(pc + 1) << endl; // Outputs the next byte in memory (assuming little-endian, might not be meaningful)
-    cout << *(pc + 2) << endl; // Outputs the byte after that
-    cout << *(pc + 3) << endl; // Outputs the byte after that
-
-    return 0;
+    cout << *(pc + 1) << endl; // Outputs the next byte in memory (assuming little-endian, might not be meaningful
 }
